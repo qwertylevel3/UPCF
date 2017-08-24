f5b3f1d9ff432e7344260c5c80af52a2583ef46c
qwertylevel3 <qwertylevel3@126.com>
2017-08-24 17:09:43 +0800

    refactor : cluster

--- util/cluster.py
+++ util/cluster.py
@@ -22,16 +22,14 @@ def run():
 
     y_meanshift= MeanShift(bandwidth=bandwidth, bin_seeding=True).fit_predict(X)
 
-    y_two_means = MiniBatchKMeans(n_clusters=3).fit_predict(X)
-
     y_ward = AgglomerativeClustering(n_clusters=3, linkage='ward',
                                            connectivity=connectivity).fit_predict(X)
     y_spectral = SpectralClustering(n_clusters=3,
                                           eigen_solver='arpack',
                                           affinity="nearest_neighbors").fit_predict(X)
-    y_dbscan = DBSCAN(eps=.2).fit_predict(X)
+    y_dbscan = DBSCAN(eps=0.5).fit_predict(X)
     y_affinity_propagation = AffinityPropagation(damping=.9,
-                                                       preference=-100).fit_predict(X)
+                                                       preference=-500).fit_predict(X)
 
     y_average_linkage = AgglomerativeClustering(
         linkage="average", affinity="cityblock", n_clusters=3,
@@ -40,22 +38,22 @@ def run():
     y_birch = Birch(n_clusters=3).fit_predict(X)
 
     plt.subplot(331)
-    plt.scatter(X[:, 0], X[:, 1], c=y_birch)
+    plt.scatter(X[:, 0], X[:, 1], c=yreal)
     plt.subplot(332)
     plt.scatter(X[:, 0], X[:, 1], c=y_kmeans)
     plt.subplot(333)
     plt.scatter(X[:, 0], X[:, 1], c=y_meanshift)
     plt.subplot(334)
-    plt.scatter(X[:, 0], X[:, 1], c=y_two_means)
-    plt.subplot(335)
     plt.scatter(X[:, 0], X[:, 1], c=y_ward)
-    plt.subplot(336)
+    plt.subplot(335)
     plt.scatter(X[:, 0], X[:, 1], c=y_spectral)
-    plt.subplot(337)
+    plt.subplot(336)
     plt.scatter(X[:, 0], X[:, 1], c=y_dbscan)
-    plt.subplot(338)
+    plt.subplot(337)
     plt.scatter(X[:, 0], X[:, 1], c=y_affinity_propagation)
-    plt.subplot(339)
+    plt.subplot(338)
     plt.scatter(X[:, 0], X[:, 1], c=y_average_linkage)
+    plt.subplot(339)
+    plt.scatter(X[:, 0], X[:, 1], c=y_birch)
     plt.show()
 
